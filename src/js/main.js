3481d50639afca2ca26e93c79360fca349383f24
Lisakova Anastasiia <a.lisakova@osla.ru>
2018-10-08 17:48:47 +0300

    manufactura desktop done

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,25 @@
 if ($('.news__slider').length) {
   $('.news__slider').slick();
 }
+
+if ($('.menu__slider').length) {
+  $('.menu__slider').slick();
+}
+
+var accordion = document.querySelectorAll(".accordion");
+
+for (var i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+// $(document).ready(function() {
+//   $("a#single_image").fancybox();
+// });
