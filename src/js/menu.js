ca21e3934f99ea16109acd06c4f23fd61b1f82e6
Lisakova Anastasiia <a.lisakova@osla.ru>
2018-10-10 18:46:02 +0300

    almost adaptive

---
+++ src/js/menu.js
@@ -0,0 +1,13 @@
+var openMenu = document.querySelector('._open-menu');
+var closeMenu = document.querySelector('._close-menu');
+var menu = document.querySelector('._menu');
+
+openMenu.addEventListener('click', function(e) {
+  e.stopPropagation();
+  menu.classList.add('active');
+});
+
+closeMenu.addEventListener('click', function(e) {
+  e.stopPropagation();
+  menu.classList.remove('active');
+});
