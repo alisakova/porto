fe30a391d267b959637a622c3449b5a9adf9fd30
alisakova <coctail_rossini@mail.ru>
2018-10-14 15:28:16 +0300

    fix mobile bugs, add href in header

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,32 @@
-if ($('.news__slider').length) {
-  $('.news__slider').slick({
+if ($(".news__slider").length) {
+  $(".news__slider").slick({
     infinite: true,
     autoplay: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     variableWidth: true,
     responsive: [
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 3,
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
       }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1
-      }
-    }
-  ]
+    ]
   });
 }
 
-if ($('.menu__slider').length) {
-  $('.menu__slider').slick();
+if ($(".menu__slider").length) {
+  $(".menu__slider").slick();
 }
-if ($('.team-slider').length) {
-  $('.team-slider').slick({
+if ($(".team-slider").length) {
+  $(".team-slider").slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     infinite: true,
@@ -48,4 +48,16 @@ for (var i = 0; i < accordion.length; i++) {
   });
 }
 
-$('.parallax-window').parallax({imageSrc: 'img/manufactura/first-slide-bg.png'});
+$(".parallax-window").parallax({
+  imageSrc: "img/manufactura/first-slide-bg.png"
+});
+
+//для плавного скролла к якорям
+$(document).ready(function() {
+  $(".nav").on("click", "a", function(event) {
+    event.preventDefault();
+    var id = $(this).attr("href"),
+        top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
+});
